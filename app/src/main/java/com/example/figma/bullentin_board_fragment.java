ff5f0fa64f629d5bf4ebe4984c6aacc7907d0064
Pjiyoung <pjy070401@naver.com>
2023-03-23 18:09:18 +0900

    bullention_board 글 DB에 넣기 완료.2023-03-23(글 불러오기 해야함)

--- app/src/main/java/com/example/figma/bullentin_board_fragment.java
+++ app/src/main/java/com/example/figma/bullentin_board_fragment.java
@@ -20,45 +20,5 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class bullentin_board_fragment extends Fragment {
-    RecyclerView recyclerView;
-    bullentin_board_adpter adpter;
 
-    bullentin_board bullentin_board;
-    ArrayList<HashMap<String, String>> fileList = new ArrayList<>();
-
-    @Override
-    public void onAttach(@NonNull Context context){
-        super.onAttach(context);
-
-        bullentin_board = (com.example.figma.bullentin_board) getContext();
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.item_main, container, false);
-
-        recyclerView = rootView.findViewById(R.id.recyclerView); //recyclerview 변수
-
-        //Linerlayoutmanger 변수에 Layoutmanager 할당 (VERTICAL은 세로 리스트, HORIZONTAL은 가로 리스트)
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-
-        //recyclerview 어댑터 설정
-        adpter = new bullentin_board_adpter(getContext());
-        recyclerView.setAdapter(adpter);
-
-        int permissionCheck1 = ContextCompat.checkSelfPermission(getContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE);//스토리지 읽기
-        if (permissionCheck1 == PackageManager.PERMISSION_GRANTED) {
-
-            fileList = bullentin_board.getFilelist(Environment.getExternalStorageDirectory().getAbsolutePath());
-
-            for (int i=0; i < fileList.size(); i++){
-                adpter.addItem(fileList.get(i));
-                adpter.notifyDataSetChanged();
-            }
-        }
-
-        return rootView;
-    }
 }
