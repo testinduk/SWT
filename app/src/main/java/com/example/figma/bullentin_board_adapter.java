79e7ce7cd0c14c52b845992c537575e8efb1ee54
Pjiyoung <pjy070401@naver.com>
2023-03-30 16:01:46 +0900

    bullention_board 리사이클러 작동.2023-03-30

--- app/src/main/java/com/example/figma/bullentin_board_adapter.java
+++ app/src/main/java/com/example/figma/bullentin_board_adapter.java
@@ -15,43 +15,118 @@ import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 
 public class bullentin_board_adapter extends RecyclerView.Adapter<bullentin_board_adapter.ViewHolder> {
+
     private ArrayList<bullentin_DB> arrayList;
     private Context context;
 
-    public bullentin_board_adapter(ArrayList<bullentin_DB> arrayList, Context context ){
-        this.arrayList = arrayList;
-        this.context =context;
-    }
 
+    public bullentin_board_adapter(ArrayList<bullentin_DB> arrayList, Context context) {
+        this.arrayList = arrayList;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public bullentin_board_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+        bullentin_board_adapter.ViewHolder holder = new bullentin_board_adapter.ViewHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull bullentin_board_adapter.ViewHolder holder, int position) {
-        holder.item_title_text.setText(arrayList.get(position).getItem_title_text());
-        holder.item_name_text.setText(arrayList.get(position).getItem_name_text());
+   //     Glide.with(holder.itemView)
+   //             .load(arrayList.get(position).getProfile())
+                //.into(holder.iv_profile);
+//        holder.tv_id.setText(arrayList.get(position).getId());
+//        holder.tv_pw.setText(String.valueOf(arrayList.get(position).getPw()));
+//        holder.tv_userName.setText(arrayList.get(position).getUserName());
+        holder.item_title_text.setText(arrayList.get(position).getTitle());
+        holder.item_name_text.setText(arrayList.get(position).getUserName());
+        holder.item_studentNumber.setText((arrayList.get(position).getStudentNumber()));
 
 
     }
 
     @Override
-    public int getItemCount() { return (arrayList != null ? arrayList.size():0);}
+    public int getItemCount() {
+        return (arrayList != null ? arrayList.size():0);
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageButton iv_profile;
+        //        Button tv_id;
+//        Button tv_pw;
+//        Button tv_userName;
         Button item_title_text;
         Button item_name_text;
+        Button item_studentNumber;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+//            this.iv_profile = itemView.findViewById(R.id.iv_profile);
+//            this.tv_id = itemView.findViewById(R.id.tv_id);
+//            this.tv_pw = itemView.findViewById(R.id.tv_pw);
+//            this.tv_userName = itemView.findViewById(R.id.tv_userName);
             this.item_title_text = itemView.findViewById(R.id.item_title_text);
             this.item_name_text = itemView.findViewById(R.id.item_name_text);
+            this.item_studentNumber = itemView.findViewById(R.id.item_studentNumber);
 
         }
     }
+//    private ArrayList<bullentin_DB> arrayList;
+//    private ArrayList<bullentin_DB> items = null;
+//    private Context context;
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//        Button item_title_text;
+//        Button item_name_text;
+//        Button item_studentNumber;
+//
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//
+//            this.item_title_text = itemView.findViewById(R.id.item_title_text);
+//            this.item_name_text = itemView.findViewById(R.id.item_name_text);
+//            this.item_studentNumber = itemView.findViewById(R.id.item_studentNumber)
+//
+//        }
+//    }
+//
+//    // 생성자
+//    bullentin_board_adapter(ArrayList<item_bullentin_board> list){
+//        items = list;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public bullentin_board_adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        Context context = parent.getContext();
+//        LayoutInflater inflater = (LayoutInflater)context.getSystemService()
+//
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_main, parent, false);
+//        ViewHolder holder = new ViewHolder(view);
+//        return holder;
+//    }
+//
+//    public bullentin_board_adapter(ArrayList<bullentin_DB> arrayList, Context context ){
+//        this.arrayList = arrayList;
+//        this.context =context;
+//    }
+//
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull bullentin_board_adapter.ViewHolder holder, int position) {
+//        holder.item_title_text.setText(arrayList.get(position).getItem_title_text());
+//        holder.item_name_text.setText(arrayList.get(position).getItem_name_text());
+//        holder.item_studentNumber.setText(arrayList.get(position).getItem_name_text());
+//
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return (arrayList != null ? arrayList.size():0);
+//    }
+
 }
