22480e66a578ddc61e55766737fb1c8cf68950ad
Pjiyoung <pjy070401@naver.com>
2023-04-11 20:56:45 +0900

    2023.04.11 pjy 주석추가 sharing_board, sharing_writing 부분

--- app/src/main/java/com/example/figma/sharing_writing.java
+++ app/src/main/java/com/example/figma/sharing_writing.java
@@ -34,7 +34,7 @@ public class sharing_writing extends Activity {
     Button btn, backButton;
     EditText edit1, edit2;
     String title, content;
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference(); // databaseReference에 저장하고 읽어옴
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,26 +48,26 @@ public class sharing_writing extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth mAuth = FirebaseAuth.getInstance();
-                String uid = mAuth.getCurrentUser().getUid();
+                FirebaseAuth mAuth = FirebaseAuth.getInstance(); //FirebaseAuth를 선언
+                String uid = mAuth.getCurrentUser().getUid(); //현재 사용자 가져오기
 
-                Intent intent = new Intent(getApplicationContext(), sharing_board.class);
+                Intent intent = new Intent(getApplicationContext(), sharing_board.class); //새로운 인텐트 객체 생성(getApplicationContext()현재 엑티비티 정보 담김, sharing_board.class 호출할 컴포넌트)
                 startActivity(intent);
 
-                Query query = databaseReference.child("sign_up").orderByChild("idToken").equalTo(uid);
+                Query query = databaseReference.child("sign_up").orderByChild("idToken").equalTo(uid); //쿼리 작성
 
                 query.addListenerForSingleValueEvent(new ValueEventListener() { //sign_up 노드 불러오기
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) { //데이터베이스 읽기
                             String emailId = dataSnapshot.child("emailId").getValue(String.class);
                             String idToken = dataSnapshot.child("idToken").getValue(String.class);
                             String studentNumber = dataSnapshot.child("studentNumber").getValue(String.class);
                             String username = dataSnapshot.child("userName").getValue(String.class);
 
 
-                            String title = edit1.getText().toString();
-                            String content = edit2.getText().toString();
+                            String title = edit1.getText().toString(); //제목을 가져옴
+                            String content = edit2.getText().toString(); //내용을 가져옴
 
 
                             DatabaseReference boardRef = databaseReference.child("sharing Board").push();
