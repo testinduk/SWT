3fa169dd3cdf8b1436c169b3fcb54259ab1fd930
KSM <rlatpals100@naver.com>
2023-05-04 14:00:57 +0900

    "메인홈수정"

--- app/src/main/java/com/example/figma/main_home.java
+++ app/src/main/java/com/example/figma/main_home.java
@@ -1,19 +1,87 @@
 package com.example.figma;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 
 public class main_home extends AppCompatActivity {
+
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private TabLayout tabLayout;
+
+    private ArrayList<String> tabNames = new ArrayList<>();
+
+    private ArrayList<String> timetable_name = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<notice_DB> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private int num_page = 6;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_home);
+
+        recyclerView = findViewById(R.id.recyclerView); //아이디 연결
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance();
+
+        databaseReference = database.getReference("notice Board");
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayList.clear();
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String uid = snapshot.getKey();
+                    notice_DB user = snapshot.getValue(notice_DB.class);
+                    arrayList.add(user);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("MainActivity", String.valueOf(databaseError.toException()));
+
+            }
+        });
+
+        adapter = new main_adapter(arrayList, this);
+        recyclerView.setAdapter(adapter);
 
 
         // (공지)더보기 버튼
@@ -82,14 +150,14 @@ public class main_home extends AppCompatActivity {
         });
 
         // 시간표 설정 버튼
-        Button btn_timetable = findViewById(R.id.btn_timetable);
-        btn_timetable.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Timetable.class);
-                startActivity(intent);
-            }
-        });
+//        Button btn_timetable = findViewById(R.id.btn_timetable);
+//        btn_timetable.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), Timetable.class);
+//                startActivity(intent);
+//            }
+//        });
 
         // 마이페이지 버튼
         ImageButton mypageButton = findViewById(R.id.mypageButton);
@@ -102,12 +170,73 @@ public class main_home extends AppCompatActivity {
             }
         });
 
-        btn_timetable.setOnClickListener(new View.OnClickListener() {
+//        btn_timetable.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), Timetable.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        // Fragment 부분
+        // ViewPager2
+        mPager = findViewById(R.id.viewpager);
+        // tabLayout 선언
+        tabLayout = findViewById(R.id.time_tab);
+
+
+        // Adapter
+        pagerAdapter = new ViewAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        /**
+         * 이 부분 조정하여 처음 시작하는 이미지 설정.
+         * 2000장 생성하였으니 현재위치 1002로 설정하여
+         * 좌 우로 슬라이딩 할 수 있게 함. 거의 무한대로
+         */
+
+        mPager.setCurrentItem(0); //시작 지점
+        mPager.setOffscreenPageLimit(6); //최대 이미지 수
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Timetable.class);
-                startActivity(intent);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+//                mIndicator.animatePageSelected(position%num_page);
             }
         });
+
+
+
+        //tabLyout와 viewPager 연결
+        new TabLayoutMediator(tabLayout, mPager, (tab, position) -> {
+
+            int index = position + 1;
+
+            String title;
+
+            if (index %2 == 0) {
+                //짝수 인덱스는 B로 끝남
+                title = (index / 2) + "B";
+            } else {
+                //홀수 인덱스는 A로 끝남
+                title = (index / 2 + 1) + "A";
+            }
+
+            tab.setText(title);
+        }).attach();
+
+
+
     }
 }
